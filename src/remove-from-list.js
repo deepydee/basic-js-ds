4391535e81b450231ca012023decc5181aa5c9f9
Vitaliy Semenov <synthex@bk.ru>
2023-04-13 19:21:25 +0600

    feat: add removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const removeKFromList = (l, k) => {
+  if (l.value === k) {
+    l = l.next;
+  }
+
+  let curr = l;
+
+  while(curr && curr.next) {
+    if (curr.next.value === k) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return l;
 }
+
+// function convertArrayToList(arr) {
+//   return arr.reverse().reduce((acc, cur) => {
+//     if (acc) {
+//       const node = new ListNode(cur);
+//       node.next = acc;
+//       return node;
+//     }
+
+//     return new ListNode(cur);
+//   }, null);
+// }
+
+// let list = convertArrayToList([3, 1, 2, 3, 4, 5]);
+
+
+// function print(list) {
+//   let current = list;
+
+//   while(current) {
+//     console.log(current.value);
+//     current = current.next;
+//   }
+// }
+
+
+// list = removeKFromList(list, 3);
+
+// print(list);
+
+
 
 module.exports = {
   removeKFromList
